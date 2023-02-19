f5c1fbf573eedfbbe7283018af9d7a855119820e
stu442 <stu442@naver.com>
2023-02-19 12:37:41 +0900

    수정##
    수정###

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
+<<<<<<< refs/remotes/origin/main
         안녕하세요!!!!
+=======
+        안녕하세요##
+>>>>>>> 수정##
         </p>
       </header>
     </div>

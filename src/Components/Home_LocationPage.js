153dcf9a2db728c01bee441d83a699d6a53f0944
happppi <likesun2000@gmail.com>
2023-03-19 01:12:46 +0900

    호텔 검색 디폴트 페이지 렌더링

--- src/Components/Home_LocationPage.js
+++ src/Components/Home_LocationPage.js
@@ -1,5 +1,30 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
 import styles from '../Pages/Home.module.css';
 function LocationPage({ imgUrl, pageName }) {
+  const navigate = useNavigate();
+
+  let postList = [];
+  async function gotoHotel(e) {
+      try {
+          const response = await axios
+          .post("http://ec2-13-209-62-189.ap-northeast-2.compute.amazonaws.com:8080/api/hotel?skip=0&take=10", {});
+          for (var i = 0; i < response.data.length; i++) {
+              postList.push(response.data[i]);
+          }
+        console.log(postList);
+        navigate('/product/search/2', {
+          state: {
+            postList: postList
+          }
+        })
+      }
+      catch(err) {
+          console.log(err);
+      }
+  }
   return (
     <div className={styles.main_link}>
               <ul>
@@ -11,7 +36,7 @@ function LocationPage({ imgUrl, pageName }) {
                 </li>
 
                 <li>
-                  <a href='/product/search/2'>
+                  <a onClick={gotoHotel}>
                     <span className={styles.hotel_img}></span>
                     <p>호텔 리조트</p>
                   </a>

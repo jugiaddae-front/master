984205ab8cb5b1d59ae07f2652f1a00cf006d582
happppi <likesun2000@gmail.com>
2023-04-02 12:31:14 +0900

    Feat: 캠핑, 게스트 하우스, 해외여행 삭제

--- src/App.js
+++ src/App.js
@@ -10,14 +10,12 @@ import Login from './Pages/Login';
 import Join from './Pages/Join'
 
 import SearchRegion from './Pages/SearchRegion';
-import DetailPage from './Pages/DetailPage';
+import DetailPage from './Pages/HotelDetailPage';
 
 import Motel from './Pages/Motel';
 import Hotel from './Pages/RoomList';
 import Pension from './Pages/Pension';
-import GuestHouse from './Pages/GuestHouse';
-import Camping from './Pages/Camping';
-import Overseas from './Pages/Overseas';
+
 import HotelDetailPage from './Pages/HotelDetailPage';
 
 
@@ -39,9 +37,7 @@ function App() {
           <Route path="/product/search/1" element={<Motel/>}></Route>
           <Route path="/product/search/2" element={<Hotel />}></Route>
           <Route path="/product/search/3" element={<Pension />}></Route>
-          <Route path="/product/search/4" element={<GuestHouse />}></Route>
-          <Route path="/product/search/5" element={<Camping />}></Route>
-          <Route path="/product/search/6" element={<Overseas />}></Route>
+
           <Route path="/product/test" element={<HotelDetailPage />}></Route>
             <Route path="/searchregion" element={<SearchRegion />}></Route>
             <Route path="/detailpage" element={<DetailPage />}></Route>
--- src/Pages/Overseas.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Overseas() {
-  return (
-    <>해외 여행 창입니다.</>
-  );
-}
-
-export default Overseas;
